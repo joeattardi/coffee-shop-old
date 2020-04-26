22e0bd3d6a9e48ff24ad4c8e154f424a3326a49c
Joe Attardi <jattardi@gmail.com>
2020-04-25 20:03:21 -0400

    Add coffee menu

---
+++ src/components/MenuCategory.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styles from './MenuCategory.module.css';
+
+export default function MenuCategory({ category }) {
+  return (
+    <div className={styles.category}>
+      <h2>{category.name}</h2>
+      <ul>
+        {category.items.map(item => (
+          <li key={item.name}>
+            <div className={styles.name}>{item.name}</div>
+            <div className={styles.price}>{item.price}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ src/templates/menu.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import MenuCategory from '../components/MenuCategory';
+
+import { graphql } from 'gatsby';
+
+import styles from './menu.module.css';
+
+export default function Menu({ data }) {
+  return (
+    <Layout>
+      <div id={styles.main}>
+        <h1>{data.markdownRemark.frontmatter.title}</h1>
+        <div id={styles.menu}>
+          {data.markdownRemark.frontmatter.categories.map(category => (
+            <MenuCategory key={category.name} category={category} />
+          ))}
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  {
+    markdownRemark(fileAbsolutePath: {regex: "/menu.md/"}) {
+      frontmatter {
+        title
+        categories {
+          name
+          items {
+            name
+            price
+          }
+        }
+      }
+    }
+  }
+`;
